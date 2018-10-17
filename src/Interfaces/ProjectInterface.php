6443c11467cd5d655a7b4bb8e03c942f06437a48
713uk13m <713uk13m@713uk13ms-MacBook-Pro.local>
2018-10-17 12:49:49 +0700

    Update vendor package> Downgrade phiên bản doctrine để hỗ trợ PHP đời thấp

--- src/Interfaces/ProjectInterface.php
+++ src/Interfaces/ProjectInterface.php
@@ -18,8 +18,8 @@ namespace nguyenanhung\MyRequests\Interfaces;
  */
 interface ProjectInterface
 {
-    const VERSION         = '0.1.3.9';
-    const LAST_MODIFIED   = '2018-10-16';
+    const VERSION         = '0.1.3.10';
+    const LAST_MODIFIED   = '2018-10-17';
     const MIN_PHP_VERSION = '5.3';
     const GET             = 'GET';
     const HEAD            = 'HEAD';
