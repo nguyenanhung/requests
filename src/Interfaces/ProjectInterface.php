b2f13477178900dca8618431e2d42b4e68088b80
713uk13m <713uk13m@713uk13ms-MacBook-Pro.local>
2018-12-13 22:52:55 +0700

    update interface

--- src/Interfaces/ProjectInterface.php
+++ src/Interfaces/ProjectInterface.php
@@ -18,8 +18,8 @@ namespace nguyenanhung\MyRequests\Interfaces;
  */
 interface ProjectInterface
 {
-    const VERSION         = '0.1.3.14';
-    const LAST_MODIFIED   = '2018-11-28';
+    const VERSION         = '0.1.3.15';
+    const LAST_MODIFIED   = '2018-12-13';
     const MIN_PHP_VERSION = '5.3';
     const GET             = 'GET';
     const HEAD            = 'HEAD';
