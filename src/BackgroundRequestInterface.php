84053370b0e8f0f3ee3122aa62619f03dd55a1b6
nguyenanhung <info@nguyenanhung.com>
2020-08-08 16:53:05 +0700

    Refactor class BackgroundRequest

--- src/BackgroundRequestInterface.php
+++ src/BackgroundRequestInterface.php
@@ -21,25 +21,27 @@ interface BackgroundRequestInterface
     /**
      * Hàm gọi 1 async GET Request để không delay Main Process
      *
-     * @author: 713uk13m <dev@nguyenanhung.com>
-     * @time  : 10/16/18 17:15
-     *
      * @param string $url Url Endpoint
      *
      * @return bool TRUE nếu thành công, FALSE nếu thất bại
+     *
+     * @author   : 713uk13m <dev@nguyenanhung.com>
+     * @copyright: 713uk13m <dev@nguyenanhung.com>
+     * @time     : 10/16/18 17:15
      */
     public static function backgroundHttpGet($url);
 
     /**
      * Hàm gọi 1 async POST Request để không delay Main Process
      *
-     * @author: 713uk13m <dev@nguyenanhung.com>
-     * @time  : 10/16/18 17:16
-     *
      * @param string $url         Url Endpoint
      * @param string $paramString Params to Request
      *
      * @return bool TRUE nếu thành công, FALSE nếu thất bại
+     *
+     * @author   : 713uk13m <dev@nguyenanhung.com>
+     * @copyright: 713uk13m <dev@nguyenanhung.com>
+     * @time     : 10/16/18 17:16
      */
     public static function backgroundHttpPost($url, $paramString = '');
 }
