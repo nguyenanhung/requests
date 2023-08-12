54ece05ecc320ec6051525be115be77890c34e2f
nguyenanhung <info@nguyenanhung.com>
2023-08-13 00:51:47 +0700

    Update Project at Sun Aug 13 00:51:47 +07 2023

--- helpers/helpers.php
+++ helpers/helpers.php
@@ -11,9 +11,9 @@ if (!function_exists('sendSimpleRequest')) {
     /**
      * Function sendSimpleRequest
      *
-     * @param string              $url    URL Target Endpoint
-     * @param string|array|object $data   Array Data to Request
-     * @param string              $method GET or POST
+     * @param string $url URL Target Endpoint
+     * @param string|array|object $data Array Data to Request
+     * @param string $method GET or POST
      *
      * @return bool|string|null
      * @author   : 713uk13m <dev@nguyenanhung.com>
@@ -23,20 +23,20 @@ if (!function_exists('sendSimpleRequest')) {
     function sendSimpleRequest($url = '', $data = array(), $method = 'GET')
     {
         $target = (!empty($data) && (is_array($data) || is_object($data))) ? $url . '?' . http_build_query($data) : $url;
-        $method = strtoupper($method);
-        $curl   = curl_init();
+        $method = function_exists('mb_strtoupper') ? mb_strtoupper($method) : strtoupper($method);
+        $curl = curl_init();
         curl_setopt_array($curl, array(
-            CURLOPT_URL            => $target,
+            CURLOPT_URL => $target,
             CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING       => "",
-            CURLOPT_MAXREDIRS      => 10,
-            CURLOPT_TIMEOUT        => 30,
-            CURLOPT_CUSTOMREQUEST  => $method,
-            CURLOPT_POSTFIELDS     => "",
-            CURLOPT_HTTPHEADER     => array(),
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_CUSTOMREQUEST => $method,
+            CURLOPT_POSTFIELDS => "",
+            CURLOPT_HTTPHEADER => array(),
         ));
         $response = curl_exec($curl);
-        $err      = curl_error($curl);
+        $err = curl_error($curl);
         curl_close($curl);
         if ($err) {
             $message = "cURL Error #: " . $err;
